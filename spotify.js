605c0abbefa0b82f87d239c3ec23bd4c4c861a12
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-12-04 15:48:38 -0500

    working spotify search

--- spotify.js
+++ spotify.js
@@ -1,18 +1,18 @@
-const spotifyKey = require("./keys")
+const spotifyKey = require('./keys')
 var Spotify = require('node-spotify-api');
  
 var spotify = new Spotify({
   id: spotifyKey.id,
   secret: spotifyKey.secret
 });
- 
+var song = process.argv.slice(2).join(' ')
+
 spotify
-  .search({ type: 'track', query: 'All the Small Things' })
+  .search({ type: 'track', query: song })
   .then(function(response) {
-    console.log(response);
+    console.log(` You want to know information about ${song} ...The artist of this song : ${response.tracks.items[0].album.artists[0].name} \nFollow this link to view a sample of the song ${response.tracks.items[0].external_urls.spotify}. \n This song was released on the ${response.tracks.items[0].album.name} album. `)
   })
   .catch(function(err) {
     console.log(err);
   });
 
-  
