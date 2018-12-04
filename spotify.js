a5234a0209b224ddf519015cd37675c935dd09ff
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-12-04 11:58:38 -0500

    updated spotfiy API

--- spotify.js
+++ spotify.js
@@ -1,20 +1,18 @@
 const spotifyKey = require("./keys")
 var Spotify = require('node-spotify-api');
  
-// var spotify = new Spotify({
-//   id: spotifyKey.id,
-//   secret: spotifyKey.secret
-// });
+var spotify = new Spotify({
+  id: spotifyKey.id,
+  secret: spotifyKey.secret
+});
  
-// // spotify
-// //   .request('https://api.spotify.com/v1/')
-// //   .then(function(data) {
-// //     console.log(data); 
-// //   })
-// //   .catch(function(err) {
-// //     console.error('Error occurred: ' + err); 
-// //   });
-// console.log(module); 
-// console.log(spotify); 
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
-console.log(spotifyKey.id, spotifyKey.secret)
+  
