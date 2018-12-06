73def529aeddc9ac7a0cde6e279d5f7e4b5c8130
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-12-05 21:05:07 -0500

    updated concert finder

--- concertfinder.js
+++ concertfinder.js
@@ -1,28 +1,24 @@
-const axios = require("axios"); 
+const axios = require("axios");
 const moment = require("moment");
-// module.exports = {
-    // concertSearch : function() {    
-    // module.exports = {
+
 module.exports = {
-    concertSearch :function() { 
-    const artist = process.argv.slice(3).join(" ");
-    const bandsURL = "https://rest.bandsintown.com/artists/"+artist+"/events?app_id=codingbootcamp"
-        axios.get(bandsURL).then(
-            function (response) {
-                // if (response === []) {
-                //     console.log(`Sorry, it looks like ${artist} has no upcoming tourdates`) {
-                
-                //      } else {
-                    for (var i =0; i < response.data.length; i++) {
+    concertSearch: function () {
+        const artist = process.argv.slice(3).join(" ");
+        const bandsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+        axios.get(bandsURL).then(function (response) {
+            if (response === []) {
+                console.log(`Sorry, it looks like ${artist} has no upcoming tourdates`)
+
+            } else {
+                for (var i = 0; i < response.data.length; i++) {
                     console.log(`${artist} will play at ${response.data[i].venue.name} in ${response.data[i].venue.region}, on ${moment(response.data[i].datetime).format('MM-DD-YYYY')}`)
                 }
-            
-            }).catch ((err) => {
+            }
+        }).catch((err) => {
             if (err) {
-            console.log(`Error: ${err}`)
-            }; 
+                console.log(`Error: ${err}`)
+            };
         })
     }
-
 }
 
--- spotify.js
+++ spotify.js
@@ -1,22 +1,22 @@
 const spotifyKey = require('./keys')
 var Spotify = require('node-spotify-api');
- 
+
 var spotify = new Spotify({
-  id: spotifyKey.id,
-  secret: spotifyKey.secret
+    id: spotifyKey.id,
+    secret: spotifyKey.secret
 });
 
-module.exports = {   
-    musicSearch : function() {
-    var song; 
-    process.argv[3] === undefined ? song = "The Sign" : song = process.argv.slice(3).join(' ') 
-    spotify.search({ type: 'track', query: song })
-  .then(function(response) {
-    console.log(` You want to know information about ${song} ...The artist of this song : ${response.tracks.items[0].album.artists[0].name} \nFollow this link to view a sample of the song ${response.tracks.items[0].external_urls.spotify}. \n This song was released on the ${response.tracks.items[0].album.name} album. `)
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
-}
+module.exports = {
+    musicSearch: function () {
+        var song;
+        process.argv[3] === undefined ? song = "The Sign" : song = process.argv.slice(3).join(' ')
+        spotify.search({ type: 'track', query: song })
+            .then(function (response) {
+                console.log(` You want to know information about ${song} ...The artist of this song : ${response.tracks.items[0].album.artists[0].name} \nFollow this link to view a sample of the song ${response.tracks.items[0].external_urls.spotify}. \n This song was released on the ${response.tracks.items[0].album.name} album. `)
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    }
 }
 
